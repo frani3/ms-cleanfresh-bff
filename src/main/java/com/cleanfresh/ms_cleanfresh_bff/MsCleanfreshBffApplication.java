b137d468facffa7758bf8881e2194f603becf819
frani <fra.barrerat@duocuc.cl>
2026-09-11 12:10:40 -0300

    feat: configuracion inicial BFF Spring Boot

---
+++ src/main/java/com/cleanfresh/ms_cleanfresh_bff/MsCleanfreshBffApplication.java
@@ -0,0 +1,13 @@
+package com.cleanfresh.ms_cleanfresh_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCleanfreshBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCleanfreshBffApplication.class, args);
+	}
+
+}
